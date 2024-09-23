4c3adb386e7e4826d2a58069ea30962b868fe373
Momoinu13 <mochansan22@gmail.com>
2024-09-23 00:41:45 -0700

    Update and rename Hand.h to Deck.h

---
+++ Blackjack/Deck.h
@@ -0,0 +1,38 @@
+#pragma once
+/*
+    Dao, Kenneth
+    Jensen, Shuma
+
+    Project: Blackjack
+    9/22/2024
+*/
+
+#include <vector>
+#include <algorithm>
+
+#include "Card.h"
+
+#ifndef DECK_H
+#define DECK_H
+
+using namespace std; 
+
+class Deck{
+    public:
+
+    Deck(); 
+
+    //functions
+    void shuffle();  // shuffles the deck of cards in the vector into a random order
+    Card deal_card(); // deals the card to the house and the player from the vector, takes the cards out of the vector 
+    void reset();  // recreates the missing cards into the deck 
+    int getDeckSize(); // returns the size of the deck (number of remaining cards)
+
+    private:
+
+    vector<Card> Cards; 
+
+}; 
+
+
+#endif 
