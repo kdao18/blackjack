70803cdd78c694b3bd9662ac69b298319a90f855
kdao18 <123984578+kdao18@users.noreply.github.com>
2024-09-24 00:35:22 -0700

    Update Player.h

--- Blackjack/Player.h
+++ Blackjack/Player.h
@@ -1,1 +1,52 @@
+#pragma once
+/*
+    Dao, Kenneth
+    Jensen, Shuma
 
+    Project: Blackjack
+    9/22/2024
+*/
+#include <vector>
+#include <algorithm>
+
+#include "Card.h"
+#include "Deck.h"
+
+#ifndef Player_H
+#define Player_H
+
+class Player
+{
+public:
+    Player();
+
+    // Add a card to the player's hand
+    void hit(const Card& card);
+
+    void stand();
+
+    int getHandValue() const;
+
+    // Get the total value of the player's hand
+    int getHandValue() const;
+
+    // Check if the player has busted (hand value > 21)
+    bool isBusted() const;
+
+    // Clear the player's hand (for starting a new round)
+    void clearHand();
+
+    // Get a string representation of the player's hand
+    std::string showHand() const;
+
+private:
+    vector<Card> Hand;
+
+    int totalScore;
+    int aceCount;
+
+    void adjustForAces();
+};
+
+
+#endif
