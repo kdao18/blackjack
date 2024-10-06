758cada6a1f13bee167fcd09fdea9ec3de71feea
kdao18 <123984578+kdao18@users.noreply.github.com>
2024-10-06 03:48:20 -0700

    Update Card.h

--- Blackjack/Card.h
+++ Blackjack/Card.h
@@ -13,13 +13,13 @@ using namespace std;
 
 class Card
 {
+    
+public:
     // Define an enumeration for card suits
-    enum class Suit { Hearts, Diamonds, Clubs, Spades };
+    enum class Suit { Hearts = 1, Diamonds = 2, Clubs = 3, Spades = 4 };
 
     // Define an enumeration for card ranks
-    enum class Rank { Ace = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Jack, Queen, King };
-public:
-    
+    enum class Rank { Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10, Ace = 11, Jack, Queen, King};
     Card(Rank r, Suit s);
 
     Suit getSuit() const;
