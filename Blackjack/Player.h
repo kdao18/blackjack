73aa68b9ec39855cfff8e33519a25a47b1120a75
Shuma Jensen <mochansan22@gmail.com>
2024-10-15 22:51:46 -0700

    Update Player.h

--- Blackjack/Player.h
+++ Blackjack/Player.h
@@ -26,10 +26,10 @@ public:
     void stand();
 
     // Get the total value of the player's hand
-    int getHandValue(const Card& initial_Hand);
+    int getHandValue(); // --> I made this mave no parameters
 
     // Check if the player has busted (hand value > 21)
-    void Busted(const Card& initial_Hand);
+    bool Busted(); // --> I made this have no parameters, I also changed this from a void function to a boolean function so that it can return a true or false. 
 
     // Clear the player's hand (for starting a new round)
     void clearHand();
@@ -37,15 +37,16 @@ public:
     // Get a string representation of the player's hand
     std::string showHand() const;
 
+    void adjustForAces(); //I moved adjustforaces into public so that I can call it in main 
+
 private:
     vector<Card> Hand;
 
     int total_score = 0;
     int aceCount;
 
-    bool isBusted = false;
 
-    void adjustForAces();
+
 };
 
 
